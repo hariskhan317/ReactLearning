d14c40f7eb33ac3d29a8510bd1cc4be4f1d02469
Haris <harisgujjar317@gmail.com>
2023-10-18 22:48:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+ 
 function App() {
+  // Age
+  // const [age, setAge] = useState(0);
+  // const increaseAge =(()=>{
+  //   const newAge = age +1 
+  //   console.log(age)
+  //   setAge(newAge);
+  // });
+
+  // input Value
+
+  // const [InputValue, setInputValue] = useState('')
+
+  // const inputHande =((event)=>{
+  //   setInputValue(event.target.value)
+  // })
+
+  const [counter , setCounter] = useState(0);
+
+  const increaseHandle = (() =>{
+    setCounter(counter + 1 )
+  })
+
+  const decreaseHandle = (() =>{
+    setCounter(counter - 1 )
+  })
+
+  const zeroHandle = (() =>{ 
+    setCounter(0)
+  }) 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {/* Age
+          <button onClick={increaseAge}>dasdasdasd</button>
+          <p>{age}</p> 
+        */}
+
+        {/* input */}
+       {/* <input type='text'  onChange={inputHande}/>
+       <p>{InputValue}</p> */}
+
+       {/* Counter app */}
+       <button onClick={increaseHandle}>Increase</button>
+       <button onClick={decreaseHandle} disabled={counter <=0}>Decrease</button>
+       <button onClick={zeroHandle}>Set Zero</button>
+
+       <p>{counter}</p>
       </header>
     </div>
   );
